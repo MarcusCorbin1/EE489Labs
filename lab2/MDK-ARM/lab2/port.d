de77795a3844249ec33c0823fcce76e5bcd5ffee
Marcus Corbin <39502121+MarcusCorbin1@users.noreply.github.com>
2020-02-03 19:43:53 -0600

    fix lab2

---
+++ lab2/MDK-ARM/lab2/port.d
@@ -0,0 +1,12 @@
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/port.c
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+lab2\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+lab2\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+lab2\port.o: ../Inc/FreeRTOSConfig.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+lab2\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ lab2/MDK-ARM/lab2/stream_buffer.d
@@ -0,0 +1,14 @@
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c
+lab2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+lab2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+lab2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+lab2\stream_buffer.o: ../Inc/FreeRTOSConfig.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+lab2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stream_buffer.h
